847395ccbcf27ad29e3d7f4903f5310c1de56f51
Botai <zhubotaigg@gmail.com>
2017-09-21 19:16:48 +0800

    update set

--- set.js
+++ set.js
@@ -1,3 +1,4 @@
+// 一组无序且唯一的项 集合
 function Set() {
     var items = {};
 
@@ -33,6 +34,67 @@ function Set() {
     this.values = function() {
         return Object.keys(items);
     }
+
+    // 并集
+    this.union = function(otherSet) {
+        var unionSet = new Set();
+        var values = this.values();
+
+        for (var i = 0; i < values.length; i++) {
+            unionSet.add(values[i]);
+        }
+
+        values = otherSet.values();
+
+        for (var i = 0; i < values.length; i++) {
+            unionSet.add(values[i]);
+        }
+
+        return unionSet;
+    }
+
+    // 交集
+    this.intersection = function(otherSet) {
+        var intersectionSet = new Set();
+        var values = this.values();
+
+        for (var i = 0; i < values.length; i++) {
+            if (otherSet.has(values[i])) {
+                intersectionSet.add(values[i]);
+            }
+        }
+        return intersectionSet;
+    }
+
+    // 差集
+    this.difference = function(otherSet) {
+        var differenceSet = new Set();
+        var values = this.values();
+
+        for (var i = 0; i < values.length; i++) {
+            if (!otherSet.has(values[i])) {
+                differenceSet.add(values[i]);
+            }
+
+        }
+        return differenceSet;
+    }
+
+    // 子集
+    this.subset = function(otherSet) {
+        if (this.size() > otherSet.size()) {
+            return false;
+        } else {
+            var values = this.values();
+            for (var i = 0; i < values.length; i++) {
+                if (!otherSet.has(values[i])) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
 }
 
 var set = new Set();
@@ -45,3 +107,52 @@ console.log(set.size());
 
 set.remove(1);
 console.log(set.values());
+
+// 集合操作 测试
+
+var setA = new Set();
+setA.add(1);
+setA.add(2);
+setA.add(3);
+
+var setB = new Set();
+setB.add(2);
+setB.add(3);
+setB.add(4);
+
+console.log("setA: " + setA.values());
+console.log("setB: " + setB.values());
+
+// 测试并集 
+var unionAB = setA.union(setB);
+console.log('并集测试： ')
+console.log(unionAB.values());
+
+// 测试交集
+var intersectionAB = setA.intersection(setB);
+console.log('交集测试： ')
+console.log(intersectionAB.values());
+
+// 测试差集
+var differenceAB = setA.difference(setB);
+console.log('差集测试： ')
+console.log(differenceAB.values());
+
+// 测试子集
+var setA = new Set();
+setA.add(1);
+setA.add(2);
+
+var setB = new Set();
+setB.add(1);
+setB.add(2);
+setB.add(3);
+
+var setC = new Set();
+setC.add(2);
+setC.add(3);
+setC.add(4);
+
+console.log('子集测试： ');
+console.log(setA.subset(setB));
+console.log(setA.subset(setC));
