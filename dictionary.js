95b3b85b02b4a542b2b4ead06eb3d5f7eeeb428e
Botai <zhubotaigg@gmail.com>
2017-09-21 20:44:31 +0800

    add dictionary，update readme

---
+++ dictionary.js
@@ -0,0 +1,15 @@
+// 字典
+function Dictionary() {
+    var items = {};
+
+    this.has = function(key) {
+        return key in items;
+    }
+
+    this.set = function(key, value) {
+        items[key] = value;
+    }
+
+
+
+}
