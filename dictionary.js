5e4374c38675607c349218f3f865311a2efe7cd9
Botai <zhubotaigg@gmail.com>
2017-09-21 21:22:28 +0800

    Add HashTable,finished dictionary

--- dictionary.js
+++ dictionary.js
@@ -10,6 +10,58 @@ function Dictionary() {
         items[key] = value;
     }
 
+    this.remove = function(key) {
+        if (this.has[key]) {
+            delete items[key];
+            return true;
+        }
+        return false;
+    }
 
+    this.get = function(key) {
+        return this.has(key) ? items[key] : undefined;
+    }
+
+    this.values = function() {
+        var values = [];
+        for (var k in items) {
+            if (this.has(k)) {
+                values.push(items[k]);
+            }
+        }
+        return values;
+    }
+
+    this.getItems = function() {
+        return items;
+    }
+
+    this.clear = function() {
+        items = {};
+    }
+
+    this.size = function() {
+        return Object.keys(items).length;
+    }
+
+    this.keys = function() {
+        var results = [];
+        for (var k in items) {
+            if (this.has(k)) {
+                results.push(k);
+            }
+        }
+        return results;
+    }
 
 }
+
+var dictionary = new Dictionary();
+dictionary.set("person1", "1111111");
+dictionary.set("person2", "222");
+dictionary.set("person3", "333");
+
+console.log(dictionary.keys());
+console.log(dictionary.values());
+console.log(dictionary.get("person1"));
+console.log(dictionary.getItems());
